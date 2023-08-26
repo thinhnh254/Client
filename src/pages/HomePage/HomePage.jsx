37fc37d23aafd670c02955a51c90c487feb5a3d2
Thinh <nguyenhungthinh2504@gmail.com>
2023-08-26 20:08:34 +0700

    home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,79 +1,57 @@
-import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import {
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import delivery from "../../assets/icon-delivery.png";
-import food from "../../assets/icon-food.png";
+import food1 from "../../assets/2minegga.png";
+import food2 from "../../assets/3miengca.png";
+import food3 from "../../assets/5miengga.png";
 import "./HomePage.scss";
 
 const HomePage = () => {
-  const count = useSelector((state) => state.counter.value);
-  const dispatch = useDispatch();
-  return (
-    <div>
-      <section className="home" id="home">
-        <div className="content">
-          <h3>
-            fresh and <span>organic</span> products for your
-          </h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum
-            ducimus quisquam voluptas minima blanditiis aliquam quis expedita
-            fugit quibusdam doloribus exercitationem similique enim, eveniet rem
-            dolorem praesentium iure incidunt voluptates?
-          </p>
-          <Link to={"#"} className="btn">
-            shop now
-          </Link>
-        </div>
-      </section>
+  const slides = [food1, food2, food3];
+  const [index, setIndex] = useState(0);
 
-      <section className="features">
-        <h1 className="heading">
-          our <span>features</span>
-        </h1>
-        <div className="box-container">
-          <div className="box">
-            <img src={food} alt="food" />
-            <h3>fresh & organic</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut
-              aperiam fugit eos et dolorem eveniet beatae facilis consequatur
-              non. Ea blanditiis minus vitae deleniti dolorum labore quasi
-              aspernatur modi ipsam!
-            </p>
-            <Link to={"#"} className="btn">
-              read more
-            </Link>
+  const handleSlideChange = (increment) => {
+    const newIndex = (index + increment + slides.length) % slides.length;
+    setIndex(newIndex);
+  };
+  return (
+    <section className="home">
+      <div className="slides-container">
+        {slides.map((slide, idx) => (
+          <div className={`slide ${index === idx ? "active" : ""}`} key={idx}>
+            <div className="content">
+              <span>fresh and organic</span>
+              <h3>upto 50% off</h3>
+              <Link to="#" className="btn">
+                shop now
+              </Link>
+            </div>
+            <div className="image">
+              <img src={slide} alt="" />
+            </div>
           </div>
-          <div className="box">
-            <img src={delivery} alt="food" />
-            <h3>free delivery</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut
-              aperiam fugit eos et dolorem eveniet beatae facilis consequatur
-              non. Ea blanditiis minus vitae deleniti dolorum labore quasi
-              aspernatur modi ipsam!
-            </p>
-            <Link to={"#"} className="btn">
-              read more
-            </Link>
-          </div>
-          <div className="box">
-            <img src={food} alt="food" />
-            <h3>easy payments</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut
-              aperiam fugit eos et dolorem eveniet beatae facilis consequatur
-              non. Ea blanditiis minus vitae deleniti dolorum labore quasi
-              aspernatur modi ipsam!
-            </p>
-            <Link to={"#"} className="btn">
-              read more
-            </Link>
-          </div>
-        </div>
-      </section>
-    </div>
+        ))}
+      </div>
+
+      <div
+        id="next-slide"
+        className="next-icon"
+        onClick={() => handleSlideChange(1)}
+      >
+        <FontAwesomeIcon icon={faChevronRight} />
+      </div>
+      <div
+        id="prev-slide"
+        className="prev-icon"
+        onClick={() => handleSlideChange(-1)}
+      >
+        <FontAwesomeIcon icon={faChevronLeft} />
+      </div>
+    </section>
   );
 };
 export default HomePage;
