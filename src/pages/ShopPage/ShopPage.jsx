8d1cabd4e0cd76a0c1d25d8988ec3e726d4b601d
Thinh <nguyenhungthinh2504@gmail.com>
2023-08-27 21:34:07 +0700

    product

--- src/pages/ShopPage/ShopPage.jsx
+++ src/pages/ShopPage/ShopPage.jsx
@@ -1,5 +1,13 @@
+import {
+  faCartPlus,
+  faEye,
+  faHeart,
+  faStar,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { Link } from "react-router-dom";
+import product from "../../assets/3miengca.png";
 import banner1 from "../../assets/icon-food.png";
 import "./ShopPage.scss";
 
@@ -43,6 +51,167 @@ const ShopPage = () => {
             <img src={banner1} alt="" />
             <h3>fresh fruits</h3>
           </Link>
+        </div>
+      </section>
+
+      <section className="products">
+        <h1 className="title">
+          our <span>products</span>
+          <Link className="link" to={"#"}>
+            view all
+          </Link>
+        </h1>
+
+        <div className="box-container">
+          <div className="box">
+            <div className="icons">
+              <Link className="icons-item" to={"#"}>
+                <FontAwesomeIcon icon={faCartPlus} />
+              </Link>
+              <Link className="icons-item" to={"#"}>
+                <FontAwesomeIcon icon={faHeart} />
+              </Link>
+              <Link className="icons-item" to={"#"}>
+                <FontAwesomeIcon icon={faEye} />
+              </Link>
+            </div>
+
+            <div className="image">
+              <img src={product} alt="" />
+            </div>
+
+            <div className="content">
+              <h3>organic food</h3>
+              <div className="price">$18.99</div>
+              <div className="stars">
+                <FontAwesomeIcon className="star-icon" icon={faStar} />
+                <FontAwesomeIcon className="star-icon" icon={faStar} />
+                <FontAwesomeIcon className="star-icon" icon={faStar} />
+                <FontAwesomeIcon className="star-icon" icon={faStar} />
+                <FontAwesomeIcon className="star-icon" icon={faStar} />
+              </div>
+            </div>
+          </div>
+
+          <div className="box">
+            <div className="icons">
+              <Link className="icons-item" to={"#"}>
+                <FontAwesomeIcon icon={faCartPlus} />
+              </Link>
+              <Link className="icons-item" to={"#"}>
+                <FontAwesomeIcon icon={faHeart} />
+              </Link>
+              <Link className="icons-item" to={"#"}>
+                <FontAwesomeIcon icon={faEye} />
+              </Link>
+            </div>
+
+            <div className="image">
+              <img src={product} alt="" />
+            </div>
+
+            <div className="content">
+              <h3>organic food</h3>
+              <div className="price">$18.99</div>
+              <div className="stars">
+                <FontAwesomeIcon className="star-icon" icon={faStar} />
+                <FontAwesomeIcon className="star-icon" icon={faStar} />
+                <FontAwesomeIcon className="star-icon" icon={faStar} />
+                <FontAwesomeIcon className="star-icon" icon={faStar} />
+                <FontAwesomeIcon className="star-icon" icon={faStar} />
+              </div>
+            </div>
+          </div>
+
+          <div className="box">
+            <div className="icons">
+              <Link className="icons-item" to={"#"}>
+                <FontAwesomeIcon icon={faCartPlus} />
+              </Link>
+              <Link className="icons-item" to={"#"}>
+                <FontAwesomeIcon icon={faHeart} />
+              </Link>
+              <Link className="icons-item" to={"#"}>
+                <FontAwesomeIcon icon={faEye} />
+              </Link>
+            </div>
+
+            <div className="image">
+              <img src={product} alt="" />
+            </div>
+
+            <div className="content">
+              <h3>organic food</h3>
+              <div className="price">$18.99</div>
+              <div className="stars">
+                <FontAwesomeIcon className="star-icon" icon={faStar} />
+                <FontAwesomeIcon className="star-icon" icon={faStar} />
+                <FontAwesomeIcon className="star-icon" icon={faStar} />
+                <FontAwesomeIcon className="star-icon" icon={faStar} />
+                <FontAwesomeIcon className="star-icon" icon={faStar} />
+              </div>
+            </div>
+          </div>
+
+          <div className="box">
+            <div className="icons">
+              <Link className="icons-item" to={"#"}>
+                <FontAwesomeIcon icon={faCartPlus} />
+              </Link>
+              <Link className="icons-item" to={"#"}>
+                <FontAwesomeIcon icon={faHeart} />
+              </Link>
+              <Link className="icons-item" to={"#"}>
+                <FontAwesomeIcon icon={faEye} />
+              </Link>
+            </div>
+
+            <div className="image">
+              <img src={product} alt="" />
+            </div>
+
+            <div className="content">
+              <h3>organic food</h3>
+              <div className="price">$18.99</div>
+              <div className="stars">
+                <FontAwesomeIcon className="star-icon" icon={faStar} />
+                <FontAwesomeIcon className="star-icon" icon={faStar} />
+                <FontAwesomeIcon className="star-icon" icon={faStar} />
+                <FontAwesomeIcon className="star-icon" icon={faStar} />
+                <FontAwesomeIcon className="star-icon" icon={faStar} />
+              </div>
+            </div>
+          </div>
+
+          <div className="box">
+            <div className="icons">
+              <Link className="icons-item" to={"#"}>
+                <FontAwesomeIcon icon={faCartPlus} />
+              </Link>
+              <Link className="icons-item" to={"#"}>
+                <FontAwesomeIcon icon={faHeart} />
+              </Link>
+              <Link className="icons-item" to={"#"}>
+                <FontAwesomeIcon icon={faEye} />
+              </Link>
+            </div>
+
+            <div className="image">
+              <img src={product} alt="" />
+            </div>
+
+            <div className="content">
+              <h3>organic food</h3>
+              <div className="price">$18.99</div>
+              <div className="stars">
+                <FontAwesomeIcon className="star-icon" icon={faStar} />
+                <FontAwesomeIcon className="star-icon" icon={faStar} />
+                <FontAwesomeIcon className="star-icon" icon={faStar} />
+                <FontAwesomeIcon className="star-icon" icon={faStar} />
+                <FontAwesomeIcon className="star-icon" icon={faStar} />
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </div>
